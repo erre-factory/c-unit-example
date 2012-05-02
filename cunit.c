7f820268318ba4a6e6ba3745bf35cf94f7894f51
erre_factory <tak.swd@gmail.com>
2012-05-02 13:45:30 +0900

    initialization change

--- cunit.c
+++ cunit.c
@@ -12,14 +12,81 @@
 
 #include "test_suites.h"
 
+enum {AUTOMATED, BASIC, CONSOLE, CURSES};
+
+struct test_options {
+	CU_BasicRunMode basic_runmode;
+	const char *filename;
+}; 
+
+struct test_interface {
+	void (*setup)(const struct test_options *);
+	void (*run_tests)(void);
+	void (*finish)(void);
+};
+
+static void automated_setup(const struct test_options *);
+static void basic_setup(const struct test_options *);
+static void dummy_setup(const struct test_options *);
+
 static void basic_run_tests(void);
+
+static void automated_finish(void);
+static void basic_finish(void);
+static void dummy_finish(void);
+
 static void usage(void);
+
+static void
+automated_setup(const struct test_options *test_opts)
+{
+
+	if (test_opts->filename != NULL) {
+		CU_set_output_filename(test_opts->filename);
+	}
+	CU_automated_enable_junit_xml(CU_TRUE);
+}
+
+static void
+basic_setup(const struct test_options *test_opts) 
+{
+
+	CU_basic_set_mode(test_opts->basic_runmode);
+}
+
+static void
+dummy_setup(const struct test_options *test_opts)
+{
+
+	;
+}
 
 static void 
 basic_run_tests(void)
 {
 
 	CU_basic_run_tests();
+}
+
+static void
+automated_finish(void)
+{
+
+	CU_list_tests_to_file();
+}
+
+static void
+basic_finish(void)
+{
+
+	CU_basic_show_failures(CU_get_failure_list());
+}
+
+static void
+dummy_finish(void)
+{
+
+	;
 }
 
 static void
@@ -45,39 +112,44 @@ usage(void)
 int 
 main(int argc, char *argv[])
 {
-	void (*run_tests)(void);
-	CU_BasicRunMode basic_runmode;
+	struct test_interface test_interfaces[] = {
+		{automated_setup,	CU_automated_run_tests,	automated_finish},
+		{basic_setup,		basic_run_tests,	basic_finish},
+		{dummy_setup,		CU_console_run_tests,	dummy_finish},
+		{dummy_setup,		CU_curses_run_tests,	dummy_finish}
+	};
+	const struct test_interface *test_if;
+	struct test_options test_opts;
 	int ch;
-	const char *filename;
 
-	run_tests = basic_run_tests;
-	basic_runmode = CU_BRM_NORMAL;
-	filename = NULL;
+	test_if = &test_interfaces[AUTOMATED];
+	test_opts.basic_runmode = CU_BRM_NORMAL;
+	test_opts.filename = NULL;
 	while ((ch = getopt(argc, argv, "abcChnp:sv")) != -1) {
 		switch (ch) {
 		case 'a':
-			run_tests = CU_automated_run_tests;
+			test_if = &test_interfaces[AUTOMATED];
 			break;
 		case 'b':
-			run_tests = basic_run_tests;
+			test_if = &test_interfaces[BASIC];
 			break;
 		case 'c':
-			run_tests = CU_console_run_tests;
+			test_if = &test_interfaces[CONSOLE];
 			break;
 		case 'C':
-			run_tests = CU_curses_run_tests;
+			test_if = &test_interfaces[CURSES];
 			break;
 		case 'n':
-			basic_runmode = CU_BRM_NORMAL;
+			test_opts.basic_runmode = CU_BRM_NORMAL;
 			break;
 		case 'p':
-			filename = optarg;
+			test_opts.filename = optarg;
 			break;
 		case 's':
-			basic_runmode = CU_BRM_SILENT;
+			test_opts.basic_runmode = CU_BRM_SILENT;
 			break;
 		case 'v':
-			basic_runmode = CU_BRM_VERBOSE;
+			test_opts.basic_runmode = CU_BRM_VERBOSE;
 			break;
 		case 'h':
 		case '?':
@@ -97,22 +169,11 @@ main(int argc, char *argv[])
 		goto cleanup;
 	}
 
-	if (run_tests == CU_automated_run_tests) {
-		if (filename != NULL) {
-			CU_set_output_filename(filename);
-		}
-		CU_automated_enable_junit_xml(CU_TRUE);
-	} else if (run_tests == basic_run_tests) {
-		CU_basic_set_mode(basic_runmode);
-	}
+	(*test_if->setup)(&test_opts);
 
-	(*run_tests)();
+	(*test_if->run_tests)();
 
-	if (run_tests == CU_automated_run_tests) {
-		CU_list_tests_to_file();
-	} else if (run_tests == basic_run_tests) {
-		CU_basic_show_failures(CU_get_failure_list());
-	}
+	(*test_if->finish)();
 
 cleanup:
 	CU_cleanup_registry();
